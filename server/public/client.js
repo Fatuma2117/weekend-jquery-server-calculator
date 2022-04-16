875c0aa87c6cda1dd9176f3e3cd3d831c2f1893f
Fatuma Abdi <Abdi.fatuma776@gmail.com>
2022-04-16 18:25:06 -0500

    pseudocoded everything

--- server/public/client.js
+++ server/public/client.js
@@ -1,8 +1,6 @@
 $(document).ready(onReady)
 
 
-
-
 function onReady(){
 $('.mathOp').on('click', mathButton)
  $('#equalButton').on('click', createEquation)
@@ -11,40 +9,33 @@ $('.mathOp').on('click', mathButton)
 
 let mathOperator;
 
-// function onEquals(){
-// let calcObject = {
-// numOne: $('#numOne').val(),
-// numTwo: $('#numTwo').val(),
-// operator: mathOperator
-// }
-// console.log(calcObject) 
-// createEquation()
-// }
-
-
-// sending object to server side calls renderHistory
+// sending calObject to server side and calls renderHistory
 function createEquation() {
   let calcObject = {
 numOne: $('#numOne').val(),
 numTwo: $('#numTwo').val(),
+// object property is the value of the global variable
 operator: mathOperator
   }
     $.ajax({
       method: 'POST',
       url: '/calculation',
+      // sending the value of the object to server
       data: calcObject
     })
     .then (function (response) {
         console.log(response);
+        // calls renderHistory when createEquation runs
         renderHistory();
       })
  
   }
-  
+  // clears inputs with button click
 function cButton(){
   $('input').val(''); 
 }
-
+// global variable mathOperator is set equal to (this) 
+//which is the math operator text when clicked. ie '+'
 function mathButton() {
 // console.log($(this).text())
 mathOperator = $(this).text()
@@ -53,16 +44,14 @@ mathOperator = $(this).text()
 
 }
 
-
+// sends get request to server and then appends response to DOM
 function renderHistory(){
   $.ajax({
     method: 'GET',
     url: '/calculation'
-  })
+  }) // uses response as a parameter, loops through response  
     .then(function (response) {
-      console.log('the server sent me something!');
       console.log(response);
-
       for (let equation of response) {
         $('#history').append(`
           <li>${equation.numOne} ${equation.operator} ${equation.numTwo} = ${equation.total}</li>
--- server/server.js
+++ server/server.js
@@ -15,12 +15,18 @@ app.listen(PORT, function() {
   console.log(`The server is running! localhost:${PORT}`);
 })
 
+
+
 let history = [];
 let calcObject;
 
 
 function endEquation (){
+
   let answer;
+//created a new object with values of num1,num2, operator, total 
+//with a new key(total) for answer value from switch 
+
   let resultObject ={
 numOne: calcObject.numOne,
 numTwo: calcObject.numTwo,
@@ -28,42 +34,44 @@ operator: calcObject.operator,
 total: answer
   }
 
+//  the value of the operator is compared with values of each case
 //
   switch (calcObject.operator) {
     case '+':
-      answer =  Number(calcObject.numOne) + Number(calcObject.numTwo)
+     answer  =  Number(calcObject.numOne) + Number(calcObject.numTwo)
       break;
     case '-':
       answer = Number(calcObject.numOne) - Number(calcObject.numTwo) 
       break;
       case '*':
-     answer = Number(calcObject.numOne) * Number(calcObject.numTwo) 
+        answer = Number(calcObject.numOne) * Number(calcObject.numTwo) 
       break;
      case '/':
       answer = Number(calcObject.numOne) / Number(calcObject.numTwo) 
        break;
   }
- history.push(resultObject)
+history.push(resultObject)
+ // push the resultObject into new array
+ console.log(history)
 }
 
-
-
-app.get('/calculation', (req, res) => {
-  console.log('GET /calculation');
-  res.send(history);
-})
-
-// receives object pushes into rew array
+// defines post route and takes data from math function
 app.post('/calculation', (req, res) => {
   console.log('POST /calculation');
   console.log(req.body)
   calcObject = req.body;
+  //calls the function that does the math calculations
 endEquation();
-  // history.push( );
+  // history.push();
+  // sends back ok 
   res.sendStatus(200);
 })
 
-
+//defined get route and sends the history array as response
+app.get('/calculation', (req, res) => {
+  console.log('GET /calculation');
+  res.send(history);
+})
 
 
 
